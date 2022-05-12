26458958349969e26760cdef3fc9075b04f1e82a
evencoding <evencoding@gmail.com>
2022-05-12 17:44:37 +0900

    tree

--- 123.py
+++ 123.py
@@ -1,12 +1,4 @@
-from bisect import bisect_left
-from sys import stdin
-input = stdin.readline
-n = int(input())
-nums = list(map(int, input().split()))
-LIS = [nums[0]]
-for i in range(1, n):
-    if nums[i] > LIS[-1]:
-        LIS.append(nums[i])
-    else:
-        LIS[bisect_left(LIS, nums[i])] = nums[i]
-print(len(LIS))
+n, k = map(int, input().split())
+for _ in range(n):
+    i, j = map(int, input().split())
+print("비와이")
